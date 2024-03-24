ed95ec5b3d05f54cf034bafc317ab1e281ee17b1
Genjimaster12 <jjllg120702@gmail.com>
2024-03-24 13:07:12 -0600

    Instalacion de jetstream para registro de usuarios y login

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,13 @@ Route::get('/', function () {
 Route::resource('/reviews', ReviewControlador::class);
 
 Route::resource('/juegos', JuegoControlador::class);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
